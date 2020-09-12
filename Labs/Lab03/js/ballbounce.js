5ae1e508d5cbf73acd5a2ef9f2709f36a1088725
Jeffrey Howard <howarjef@iu.edu>
2020-09-11 21:47:31 -0400

    Added Bouncing Ball Page

--- Labs/Lab03/js/ballbounce.js
+++ Labs/Lab03/js/ballbounce.js
@@ -1,18 +1,28 @@
-var x = 50;
-var y = 50;
-var mouseX = x;
-var mouseY = y;
-moveUp=
+let x; // x coordinate
+let y; // y coordinate
+let xSpeed; // Speed set for x coordinate
+let ySpeed; // Speed set for y coordinate
 
 function setup() {
-  createCanvas(400, 300);
+  createCanvas(800, 600); // Canvas size required
+  x = 400;
+  y = 300;
+  xSpeed = 10;
+  ySpeed = 10;
 }
 
 function draw() {
-  background(153, 204, 255);
-  fill(102, 255, 102);
-  ellipse(mouseX, mouseY, 50, 50);
+  background(0, 255, 255);
+  fill(0, 255, 158); // Color of ball
+  circle(x, y, 80, 60); // Ball direction and size set
 
-  if()
-  x = x + 1;
+  x = x + xSpeed;
+  y = y + ySpeed;
+
+  if (x + 80 == width || x == 0) {
+    xSpeed = xSpeed * -1;
+  }
+  if (y + 60 == height || y == 0) {
+    ySpeed = ySpeed * -1;
+  }
 }
